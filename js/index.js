ddb1918b999af239365dfb45dfca9d176fb5b271
Jeff Mignone <jeff.mignone@gmail.com>
2018-04-22 21:57:08 -0400

    cleaning up code

--- js/index.js
+++ js/index.js
@@ -1,157 +1,69 @@
-/* Functions should either do something (modify) or answer something (query), but not both. */
-
-/* STATE MODEL OR JUST VARIABLES */
-
-let _nextToken; 
-let _prevToken; 
-let _apiData;
-let _currentTotal = 0;
-let _searchWord;
-let _currentVideo;
-
-const setNextToken = (token) => _nextToken = token;
-const setPrevToken = (token) => _prevToken = token;
-const setSearchWord = () => _searchWord = $('#searchField').val();
-const setCount = (results, sign) => _currentTotal += results; 
-const setApiData = (data) => _apiData = data;
-const setVideo = (video) => _currentVideo = video;
-
-
-/* CACHED DOM ELEMENTS */
-
-const $search = $('#searchBtn');
+// STATE
+const state = {
+    apiData: {},  
+    currentVideo: {}, // overlay
+    currentPage: '',
+    searchWord: '', // navTo, ajax
+};
+// CACHED DOM ELEMENTS
+const $search = $('[rel="js-search-btn"]');
 const $body = $('body');
 const $container = $('.container');
-const $header = $('header');
-const $nav = $('.nav');
 
+// SET FUNCTIONS
+const setTokens = ({ nextPageToken, prevPageToken } = {}) => { 
+    state.nextPageToken = nextPageToken,
+    state.prevPageToken = prevPageToken 
+};
+const setSearchWord = () => state.searchWord = $('#searchField').val();
+const setApiData = apiData => state.apiData = apiData;
+const setPage = page => state.currentPage = page;
+const setVideo = video => state.currentVideo = video;
 
-
-
-const getDataFromAPI = ({ maxResults = 24, 
-                          type = 'video',
-                          channelId = undefined,
-                          q = undefined,
-                          pageToken = undefined } = {}) => {
-
+// API FUNCTION
+const getDataFromAPI = ({ q, channelId, pageToken } = {}) => {
     const settings = {
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: { 
           part: 'snippet',
           key: '#',
-          maxResults,
+          maxResults: 24,
+          type: 'video',
           q,
           pageToken,
-          channelId,
-          type
+          channelId
         },
         dataType: 'json',
         type: 'GET',
         success: setDataSuccess,
-        error: setDataError
+        error: logError
     }
     $.ajax(settings);
 };
 
-const searchBtnClick = () => {
-    $search.on('click', e => {
-        e.preventDefault();
-        setPage('main');    
-        setSearchWord();
-        getDataFromAPI({ q: _searchWord });
-    });
-};
-
-const setPage = (page) => {
-    if (page === 'main')  {
-        $body.addClass('main')
-        $body.removeClass('channel');
-    } else if (page === 'channel') {
-        $body.addClass('channel');
-        $body.removeClass('main');
-    }
-}
-
-const getPage = () => {
-    return $body.hasClass('main') ? 'main' : 'channel';
-}
-
-function videoClick() {
-    $body.on('click', '.ytlink', function(e) {
-        e.preventDefault();
-        let currentVideo = _apiData.items[$(this).data('idx')];
-        setVideo(currentVideo);
-        openOverlay($(this));
-    });
-}
-
-const closeClick = () => {
-    $body.on('click', '.close', () => removeOverlay());
-}
-
-const channelClick = () => {
-    $body.on('click', '.btn.channel', function() {
-        setPage('channel');
-        getDataFromAPI({ channelId: _currentVideo.snippet.channelId });
-        removeOverlay();
-    });
-}
-
-const removeOverlay = () => {
-    $('.overlay').remove();
-    $body.removeClass('overlayOn');
-}
-
+// SUCCESS/ERROR CALLBACKS
 function setDataSuccess(data) {
-  _currentTotal === 0 && setCount(data.pageInfo.resultsPerPage);
-  setNextToken(data.nextPageToken);
-  setPrevToken(data.prevPageToken);
-  setApiData(data);
-
-  createHeader();
-  createResults(data.pageInfo.totalResults);
-  createGrid(data, 3);
-  createNavBtns();
+  setTokens({ nextPageToken: data.nextPageToken, prevPageToken: data.prevPageToken });
+  setApiData(data.items);
+  createGrid(data, 4);
+  setUpNavBtns();
 };
 
-function setDataError(err) {
-    console.log('err: ', err);
-};
+const logError = (err) => console.log('err: ', err);
 
-const createHeader = () => {
-    getPage() === 'main' ? txt = 'Thinkful Tube Search' : txt = `Channel: ${_currentVideo.snippet.channelTitle}`;
-    $header.html(`<h1 class="text-center">${txt}</h1>`);
-};
-
-const openOverlay = (link) => {
-    let video = link.attr('href');
-    let overlay = `<section class='overlay'>
-                    <div class='outer-wrapper'>
-                            <h1 class="text-center videoTitle">${_currentVideo.snippet.title}</h1>
-                            <div class='wrapper'>
-                                <iframe src="${video}"></iframe>
-                            </div>
-                            <section class='btn-container'>
-                                <button class='btn channel'>MORE FROM THIS CHANNEL</button>
-                                <button class="close">CLOSE</button>
-                            </section>
-                        </div>
-                    </section>`;
-    $body.append(overlay);
-    $body.addClass('overlayOn');
-}
-
+// SET UP PAGE
 const createGrid = (data, cols) => {
-    console.log(data);
     let columns = '';
     let rows = '';
 
     data.items.forEach((video, i) => {
       columns+=`<div class="col-${cols}">
-                <a class="ytlink" href="https://www.youtube.com/embed/${video.id.videoId}" data-idx="${i}">
-                    <div class="bg" style="background-image: url(${video.snippet.thumbnails.medium.url})">
-                        <div class="overlayVid">
-                            <img src="img/play-button.svg" class="playBtn" alt="play button">         
+                <a class="ytlink" href="https://www.youtube.com/embed/${video.id.videoId}" rel="video">
+                    <div class="contain">
+                        <div class="bg" style="background-image: url(${video.snippet.thumbnails.medium.url})">
+                            <div class="overlayVid">
+                                <img src="img/play-button.svg" class="playBtn" alt="play button">         
+                            </div>
                         </div>
                     </div>
                     <div class="title">
@@ -171,30 +83,77 @@ const createGrid = (data, cols) => {
     $container.html(view);
 };
 
+const openOverlay = () => {
+    const location = state.currentVideo.snippet;
+    let overlay = `<section class='overlay'>
+                    <div class='outer-wrapper'>
+                            <h1 class="text-center videoTitle">${location.channelTitle}</h1>
+                            <div class='wrapper'>
+                                <iframe src="${`https://www.youtube.com/embed/${state.currentVideo.id.videoId}`}"></iframe>
+                            </div>
+                            <section class='btn-container text-center'>
+                                <button class='btn channel'>MORE FROM THIS CHANNEL</button>
+                                <button class="close">CLOSE</button>
+                            </section>
+                            <h2>DESCRIPTION</h2>
+                            <div class="description">${location.description}</div>
+                        </div>
+                    </section>`;
+    $body.append(overlay);
+    setCSS('hidden');
+}
 
+const setUpNavBtns = () => {
+    $('[rel="js-controls"] > [rel*="js-"]').hide();
+    state.prevPageToken && $('[rel="js-prev"]').show();
+    state.nextPageToken && $('[rel="js-next"]').show();
+}
+
+// navTo FUNCTIONS
+const getToken = (el) => $(el).attr('rel') === 'js-prev' ? state.prevPageToken : state.nextPageToken;
+
+const setArgs = (pageToken) => {
+    return state.currentPage === 'main' ? { q: state.searchWord, pageToken } : { channelId: state.currentVideo.channelId, pageToken };
+}
+
+// EVENT LISTENERS
 const navTo = () => {
-  $body.on('click', '.nav-btn', e => {
-    numResults = _apiData.pageInfo.resultsPerPage;
-    let moveTo = $(e.target).hasClass('prev') ? _prevToken : _nextToken;
-    moveTo === _prevToken ? setCount(-numResults) : setCount(numResults);
-    let args = { pageToken: moveTo };
-    getPage() === 'main' ? args.q =  _searchWord : args.channelId = _currentVideo.snippet.channelId; 
-    getDataFromAPI(args);
+    $body.on('click', '[rel="js-controls"]', e => {
+        let currToken = getToken(e.target);
+        let args = setArgs(currToken);
+        getDataFromAPI(args);
+    });
+};
+  
+const searchBtnClick = () => {
+    $search.on('click', e => {
+        e.preventDefault();
+        setSearchWord();
+        getDataFromAPI({ q: state.searchWord });
+    });
+};
+
+const videoClick = () => {
+  $body.on('click', '[rel="video"]', e => {
+    e.preventDefault();
+    let idx = $('[rel="video"]').index( $(e.currentTarget) );
+    setVideo(state.apiData[idx]);
+    openOverlay();
   });
-};
+}
 
-const createResults = (totalResults) => {
-    $('.results').html(`<h1 class="text-center">${_currentTotal} out of ${totalResults} results</h1>`);
-};
+const setCSS = (val) => $body.css('overflow', val);
 
-const createNavBtns = () => {
-    $nav.empty();
-    _prevToken && $nav.append('<button class="nav-btn prev">PREV</button>');
-    _nextToken && $nav.append('<button class="nav-btn next">NEXT</button>');
-};
+const removeOverlay = () => $body.on('click', '.close', () => {
+    $('.overlay').remove();
+    setCSS('initial');
+});
 
-searchBtnClick();
-videoClick();
-navTo();
-channelClick();
-closeClick();
+$(
+setPage('main'),
+setUpNavBtns(),
+searchBtnClick(),
+videoClick(),
+navTo(),
+removeOverlay()
+);
